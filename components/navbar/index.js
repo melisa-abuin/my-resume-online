b8198c74d9d7589789ffb1cb80d868c942b02d94
Melisa Abuin <meabuin@alumno,unlam.edu.ar.com>
2020-08-02 12:22:15 -0300

    add modal behaviour

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,15 +1,24 @@
 import { Container, Item } from './styles'
+import React, { useState } from 'react'
+import { Modal } from '../'
 import Link from 'next/link'
 
 export default function Navbar() {
+  const [ modal, setModal ] = useState(0)
+
+  function handleClick(modal) {
+    setModal(!modal)
+  }
+  
   return (
-    <Container>
-      <Link href="/" passHref>
-        <Item>About Me</Item>
-      </Link>
-      <Link href="/" passHref>
-        <Item>Download PDF resume</Item>
-      </Link>
-    </Container>
+    <>
+      <Container>
+        <Item onClick={() => handleClick(modal)}>About Me</Item>
+        <Link href="/" passHref>
+          <Item>Download PDF resume</Item>
+        </Link>
+      </Container>
+      {modal && <Modal modalState={modal} onClick={handleClick} />}
+    </>
   )
 }
