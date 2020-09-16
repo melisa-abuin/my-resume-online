f2b83f8286152ac983df7e24f5c77a1cda86c527
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-09-15 22:29:14 -0300

    feature/MRO-19: add media queries for education section

--- components/education-section/images-grid/styles.js
+++ components/education-section/images-grid/styles.js
@@ -1,49 +1,81 @@
 import styled, { css } from 'styled-components'
+import MediaQueries from '../../media-queries'
 
 export const Title = styled.h2`
 `
 export const Selected = styled.div`
-  justify-content: space-between;
+  justify-content: center;
   position: absolute;
   display: flex;
   flex-direction: row;
   height: ${({ visible }) => visible ? '100%' : '0%'};
   opacity: ${({ visible }) => visible ? '1' : '0'};
-  transition: height 0s, opacity 3s;
+  transition: height 0s, opacity 1s;
+
+  ${MediaQueries.mobile`
+    flex-direction: column;
+    justify-content: flex-start;
+  `};
 `
 export const Paragraph = styled.p`
   max-width: 90%;
+  display: inline;
 `
-export const Carousel = styled.p`
+export const Carousel = styled.div`
   position: relative;
   width: 100%;
   height: 350px;
+  
+  ${MediaQueries.mobile`
+    height: 700px;
+  `};
 `
-export const Rectangle = styled.div`
-  position: absolute;
-  width: 100%;
+export const Circle = styled.div`
+  display: flex;
+  border-radius: 50%;
+  width: 420px;
   z-index: 0;
-  height: 350px;
-  top: -20px;
-  background-color: ${({ theme }) => theme.colors.black};
+  height: 420px;
+  justify-content: center;
+  align-items: center;
+  border: 5px solid ${({ theme }) => theme.colors.black};
+  border-width: 1px 0 1px 10px;
+
+  ${MediaQueries.mobile`
+    width: 90vw;
+    z-index: 0;
+    height: 250;
+  `};
 `
-export const LinearRectangle = styled.div`
-  position: absolute;
-  width: 150px;
+export const LinearCircle = styled.div`
+  display: flex;
+  border-radius: 50%;
+  width: 390px;
   z-index: 0;
-  height: 150px;
-  top: -40px;
-  right: -20px;
+  height: 390px;
   border: 5px solid ${({ theme }) => theme.colors.aquamarine};
-  background-color: ${({ theme }) => theme.colors.transparent};
+  justify-content: center;
+  align-items: center;
+  border-width: 1px 10px 1px 0;
+
+  ${MediaQueries.mobile`
+    width: 90vw;
+    z-index: 0;
+    height: 250;
+  `};
 `
 export const Image = styled.img`
-  width: 95%;
+  width: 350px;
   height: 350px;
+  border-radius: 50%;
   z-index: 1;
   position: relative;
-  top: 0;
-  left: -10px;
+
+  ${MediaQueries.mobile`
+    width: 80vw;
+    z-index: 0;
+    height: 250;
+  `};
 `
 export const Container = styled.div`
   width: 100%;
@@ -54,10 +86,22 @@ export const Container = styled.div`
   padding: 0 60px;
   box-sizing: border-box;
   justify-content: space-between;
+
+  ${MediaQueries.mobile`
+    justify-content: center;
+    padding: 0 10px;
+  `};
+
 `
 export const Column = styled.div`
   max-width: 50%;
   position: relative;
+
+  ${MediaQueries.mobile`
+    text-align: center;
+    max-width: 100%;
+    margin-bottom: 40px;
+  `};
 `
 export const Arrow = styled.img`
   height: 60px;
@@ -73,6 +117,18 @@ export const Arrow = styled.img`
       left: -12px;
       transform: rotate(180deg)`
   };
+
+  ${MediaQueries.mobile`
+    top: unset;
+    bottom: 0;
+
+    ${({ right }) => right ? 
+      css` 
+        right: 20%;` : 
+      css` 
+        left: 20%;`
+    };
+  `};
 `
 export const Wrapper = styled.div`
   margin-top: 100px;
@@ -80,4 +136,8 @@ export const Wrapper = styled.div`
   padding: 50px 0;
   display: flex;
   justify-content: center;
+  
+  ${MediaQueries.mobile`
+    overflow: hidden;
+  `};
 `
