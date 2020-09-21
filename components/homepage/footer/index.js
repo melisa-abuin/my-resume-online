7d6dc82b1e98cd9d9cace9de2b7b0040c0b6ea57
Melisa Abuin <meabuin@alumno.unlam.edu.ar>
2020-09-21 16:47:12 -0300

    Bugfix/mro-21: minor fixes to media queries (#36)
    * added new style to arrows on mobile version
    
    * prevent text from overlapping image

--- components/homepage/footer/index.js
+++ components/homepage/footer/index.js
@@ -1,20 +1,23 @@
 import { Container, Item, Link, LinkImage } from './styles'
+import { useTranslation } from 'react-i18next'
 
 export default function Footer() {
-    return (
-      <Container>
-        <Link href="https://www.linkedin.com/in/melisa-abuin-99273b111/">
-          <LinkImage src='/linkedin-icon.png'/> 
-          <Item>My profile </Item>
-        </Link>
-        <Link href="https://github.com/Mel-A">
-          <LinkImage src='/github-icon.png'/> 
-          <Item>Repository </Item>
-        </Link>
-        <Link href="mailto:melisa.abuin@gmail.com?Subject=Website%20visitor">
-          <LinkImage src='/gmail-icon.png'/> 
-          <Item>melisa.abuin@gmail.com </Item>
-        </Link>
-      </Container>
-    )
+  const { t } = useTranslation('common')
+  
+  return (
+    <Container>
+      <Link href="https://www.linkedin.com/in/melisa-abuin-99273b111/">
+        <LinkImage src='/linkedin-icon.png'/> 
+        <Item>{t("general.profile")}</Item>
+      </Link>
+      <Link href="https://github.com/Mel-A">
+        <LinkImage src='/github-icon.png'/> 
+        <Item>{t("general.repository")}</Item>
+      </Link>
+      <Link href="mailto:melisa.abuin@gmail.com?Subject=Website%20visitor">
+        <LinkImage src='/gmail-icon.png'/> 
+        <Item>{t("general.mail-user")}</Item>
+      </Link>
+    </Container>
+  )
 }
