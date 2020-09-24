fd91bb510d7bd4080bf44b78873d4820bef05af8
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-09-24 13:43:03 -0300

    adding sizes to letters

--- pages/_app.js
+++ pages/_app.js
@@ -21,9 +21,10 @@ function MyApp({ Component, pageProps }) {
   return <ThemeProvider theme={theme}>
           <Head>
             <title>Abuin Melisa</title>
+            <meta property="og:site_name" content='Abuin Melisa' />
             <meta property='og:title' content='Abuin Melisa' key='title' />
             <meta property='og:description' content='A brief recap of my experience and knowledge in a web application' key='description' />
-            <meta property='og:image' content='https://my-resume-online.vercel.app/logo.png' key='image' />
+            <meta property='og:image' itemprop='image' content='https://my-resume-online.vercel.app/logo.png' key='image' />
             <link rel="shortcut icon" href="/logo.png" />
           </Head>
           <I18nextProvider i18n={i18next}>
