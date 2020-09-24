a0be7c3b8a090e52c3f36b366d1dbce70418fb38
Melisa Abuin <meabuin@alumno.unlam.edu.ar>
2020-09-24 12:20:17 -0300

    Feature/mro-35: change arrows to a new comp (#39)
    * move arrows to a new component
    
    * add metadata tags

--- components/education-section/images-grid/styles.js
+++ components/education-section/images-grid/styles.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import MediaQueries from '../../media-queries'
 
 export const Title = styled.h2`
@@ -112,50 +112,6 @@ export const Column = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-  `};
-`
-export const Arrow = styled.img`
-  height: 60px;
-  width: 60px;
-  ${({ right }) => !right && css`transform: rotate(180deg);`}
-
-  ${MediaQueries.mobile`
-    height: 30px;
-    width: 30px;
-  `};
-`
-export const Swipper = styled.div`
-  position: absolute;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  z-index: 2;
-  top: calc(50% - 10px);
-
-  ${({ right }) => right ? 
-    css` 
-      right: -12px;` : 
-    css` 
-      left: -12px;`
-  };
-
-  ${MediaQueries.mobile`
-    top: unset;
-    bottom: -10px;
-
-    ${({ right }) => right ? 
-      css` 
-        right: 15%;` : 
-      css` 
-        left: 15%;`
-    };
-  `};
-`
-export const Indicator = styled.label`
-  display: none;
-  margin: 0 20px;
-  ${MediaQueries.mobile`
-    display: inline;
   `};
 `
 export const Wrapper = styled.div`
