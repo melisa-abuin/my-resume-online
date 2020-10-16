005c54b8c61248c6cc1c3dd009491252c4d63ff5
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-10-16 20:12:10 -0300

    adding first background approach

--- components/work-experience-section/background/styles.js
+++ components/work-experience-section/background/styles.js
@@ -6,11 +6,38 @@ export const Container = styled.div`
   max-width: 1300px;
   padding: 80px 0;
   height: 450px;
+  z-index: 1;
+  position: absolute;
   box-sizing: border-box;
 `
 export const Wrapper =  styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+  height: 400px;
+  position: relative;
+
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+    z-index: -1;
+    background-image:
+      linear-gradient(white 3%, transparent 3%),
+      linear-gradient(to right, grey 97%, white 9%);
+    background-size: 40px 40px;
+  }
+  &::after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+    z-index: -1;
+    background-image: linear-gradient(to top,white 30%, transparent  80%);
+  }
 `
