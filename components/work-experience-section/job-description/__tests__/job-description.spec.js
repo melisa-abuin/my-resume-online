81c873b79607d5cfa415edc03123b140bc42dd06
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-10-17 01:16:48 -0300

    adding tests

--- components/work-experience-section/job-description/__tests__/job-description.spec.js
+++ components/work-experience-section/job-description/__tests__/job-description.spec.js
@@ -1,9 +1,30 @@
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import JobDescription from '..'
+import { ThemeProvider } from "styled-components"
+import theme from '../../../../theme'
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({t: key => key})
+}))
+
+const dummyData = {
+  title: 'some title',
+  description: 'some description'
+}
 
 describe('jobDescription', () => {
 it('jobDescription renders correctly', () => {
-    const jobDescription = shallow(<JobDescription />)
+    const jobDescription = shallow(<JobDescription {...dummyData} />)
+    expect(jobDescription).toMatchSnapshot()
+  })
+
+  it('jobDescription renders theme correctly', () => {
+    const jobDescription = mount(<JobDescription {...dummyData} />, {
+      wrappingComponent: ThemeProvider,
+      wrappingComponentProps: {
+        theme,
+      },
+    })
     expect(jobDescription).toMatchSnapshot()
   })
 })
