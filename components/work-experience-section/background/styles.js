fe3c1f8db2c2f49e0f3847fa9e12364f12ba5a0a
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-10-18 00:02:29 -0300

    add media queries for w.e. section

--- components/work-experience-section/background/styles.js
+++ components/work-experience-section/background/styles.js
@@ -3,14 +3,15 @@ import styled from 'styled-components'
 import { PageSection } from '../../common'
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 1300px;
   z-index: 1;
   flex-direction: row;
   align-items: center;
   display: flex;
-  margin-left: 20px;
   box-sizing: border-box;
+
+  ${MediaQueries.mobile`
+    flex-direction: column;
+  `};
 `
 
 export const Group = styled.div`
@@ -22,13 +23,15 @@ export const Image = styled.img`
   height: 250px;
   width: 250px;
   box-shadow: 0px 2px 40px 1px ${({ theme }) => theme.colors.gray};
+
+  ${MediaQueries.mobile`
+    height: 150px;
+    width: 150px;
+  `};
 `
 export const Wrapper =  styled(PageSection)`
-  width: 100%;
   position: relative;
   padding: 80px 0;
-  background-color: #cce5ff;
-  display: flex;
+  background-color:  ${({ theme }) => theme.colors.onahau};
   overflow: hidden;
-  justify-content: center;
 `
