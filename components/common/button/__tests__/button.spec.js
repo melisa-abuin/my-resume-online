1144f2aab7f6e8065a7e032483a84cd1db357cc2
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-10-24 14:56:47 -0300

    increased coverage to 100% for button

--- components/common/button/__tests__/button.spec.js
+++ components/common/button/__tests__/button.spec.js
@@ -1,10 +1,37 @@
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import Button from '..'
+import { StyledButton } from '../styles'
+import { ThemeProvider } from "styled-components"
+import theme from '../../../../theme'
+
+const dummyText = 'text'
+const onClick =  jest.fn()
 
 describe('Button', () => {
   it('button renders correctly', () => {
-    const dummyText = 'text'
+    
     const button = shallow(<Button text="text">{dummyText}</Button>)
     expect(button).toMatchSnapshot()
+  })
+
+  it('button renders theme correctly', () => {
+    const button = mount(<Button text="text">{dummyText}</Button>, {
+      wrappingComponent: ThemeProvider,
+      wrappingComponentProps: {
+        theme,
+      },
+    })
+  })
+
+  it('onClick method', () => {
+    const button = mount(<Button text="text" onClick={onClick}>{dummyText}</Button>, {
+      wrappingComponent: ThemeProvider,
+      wrappingComponentProps: {
+        theme,
+      },
+    })
+    button.find(StyledButton).simulate('click');
+
+    expect(onClick).toHaveBeenCalled()
   })
 })
